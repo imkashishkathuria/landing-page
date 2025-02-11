26ce099f7fed39df6dbb69a7c6567bee718a0b84
Kashish <kathuriak805@gmail.com>
2025-02-11 15:20:17 +0530

    hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 // import { Header } from "@/sections/Header";
 
 import { Header } from "../sections/Header";
+import { Hero } from "../sections/Hero";
 
 export default function Home() {
   return (
-    
+    <>
    <Header />
+   <Hero />
+   </>
   );
 }
